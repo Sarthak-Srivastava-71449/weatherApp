94baf5c8cb6a60eb3dd738eec64b9a3fa1a05f8a
Sarthak Srivastava <sarthaksri2003@gmail.com>
2024-04-02 15:06:14 +0530

    CORS origin set to all

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
